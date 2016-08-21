97a22f9d916e3dd5c7009684e3a04e46d1bbe943
Paul Wilson <paulanthonywilson@gmail.com>
2016-08-21 16:16:45 +0100

    tidy

--- apps/cb_slack/lib/cb_slack.ex
+++ apps/cb_slack/lib/cb_slack.ex
@@ -5,7 +5,6 @@ defmodule CbSlack do
     import Supervisor.Spec, warn: false
 
     children = [
-      # worker(Restarter, [{Cb.Bot, :start_link, [@slack_token, [name: :slack_bot]]}, 10_000, [name: :slack_bot_restarter]] ),
       worker(Cb.BotStarter, [])
     ]
 
