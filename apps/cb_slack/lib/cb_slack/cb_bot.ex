587e62a7c06ffb4f3861e2899c82f43069706456
Paul Wilson <paulanthonywilson@gmail.com>
2016-08-22 17:11:57 +0100

    refactor

--- apps/cb_slack/lib/cb_slack/cb_bot.ex
+++ apps/cb_slack/lib/cb_slack/cb_bot.ex
@@ -12,16 +12,18 @@ defmodule Cb.Bot do
   def show_inet_addr(_bot, msg) do
     case :inet.getifaddrs do
       {:ok, addrs} ->
-        reply =  addrs
-      |> Enum.map(fn {iface, properties} -> { List.to_string(iface), Keyword.get(properties, :addr) } end)
-      |> Enum.map(fn {iface, {a1,a2,a3,a4}} -> "#{iface}: #{a1}.#{a2}.#{a3}.#{a4}"
-      _ -> nil end)
-      |> Enum.filter(&(&1))
-      |> Enum.join("\n")
-      say self, msg["channel"], reply
-
+        say self, msg["channel"], addrs_to_msg(addrs)
       {:error, posix} ->
         say self, msg["channel"], "That did not work out #{inspect(posix)}"
     end
+  end
+
+  defp addrs_to_msg(addrs) do
+    addrs |> Enum.map(fn {iface, properties} -> { List.to_string(iface), Keyword.get(properties, :addr) } end)
+    |> Enum.map(
+      fn {iface, {a1,a2,a3,a4}} -> "#{iface}: #{a1}.#{a2}.#{a3}.#{a4}"
+      _ -> nil end)
+    |> Enum.filter(&(&1))
+    |> Enum.join("\n")
   end
 end
