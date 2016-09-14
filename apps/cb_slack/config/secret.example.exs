7b9afae1ba61602eec5020b966d08471e68b7273
Paul Wilson <paulanthonywilson@gmail.com>
2016-09-14 16:19:16 +0100

    example secret files

---
+++ apps/cb_slack/config/secret.example.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :cb_slack, :slack_token, "YOUR SLACK TOKEN"
---
+++ apps/fw/config/secret.example.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :fw, :wifi_opts, ssid: "YOUR SSID", key_mgmt: :"WPA-PSK", psk: "YOUR WIFI PASSWORD"
