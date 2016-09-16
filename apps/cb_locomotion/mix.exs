44c6cdb1af155b8b85fcccaa69205e489e751619
Paul Wilson <paulanthonywilson@gmail.com>
2016-09-16 11:50:35 +0100

    No need to incorporate Nerves in cb_locomotion

--- apps/cb_locomotion/mix.exs
+++ apps/cb_locomotion/mix.exs
@@ -1,32 +1,25 @@
 defmodule CbLocomotion.Mixfile do
   use Mix.Project
 
-  @target System.get_env("NERVES_TARGET") || "rpi"
-
   def project do
     [app: :cb_locomotion,
-     version: "0.0.1",
-     target: @target,
-     archives: [nerves_bootstrap: "~> 0.1.4"],
-     deps_path: "../../deps",
-     build_path: "../../_build/#{@target}",
+     version: "0.1.0",
+     build_path: "../../_build",
      config_path: "../../config/config.exs",
+     deps_path: "../../deps",
      lockfile: "../../mix.lock",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     aliases: aliases(Mix.env),
-     deps: deps ++ system(@target, Mix.env)]
+     deps: deps]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [mod: {CbLocomotion, []},
      applications: applications(Mix.env)]
   end
 
-  def deps do
+  defp deps do
     [
       {:nerves, "~> 0.3.0"},
       {:elixir_ale, "~> 0.5.6", only: :prod},
@@ -34,21 +27,8 @@ defmodule CbLocomotion.Mixfile do
     ]
   end
 
-  def system(target, :prod) do
-    [{:"nerves_system_#{target}", ">= 0.0.0"}]
-  end
-  def system(_,_), do: []
-
   defp applications(:prod), do: [:elixir_ale | general_apps]
   defp applications(_), do: general_apps
 
   defp general_apps, do: [:logger]
-
-
-  def aliases(:prod) do
-    ["deps.precompile": ["nerves.precompile", "deps.precompile"],
-     "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
-  end
-  def aliases(_), do: []
-
 end
